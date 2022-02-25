aa1d525f7bb20b58b7250c77fdc8e54034aa6efa
Wellingtongg <wellingtongg12@gmail.com>
2022-02-24 21:00:00 -0300

    fix weather test

--- spec/controller/weather_controller_spec.rb
+++ spec/controller/weather_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe WeatherController, type: :controller do
       post :tweet, params: { q: 'london', units: 'metric', lang: 'pt_br', appid: ENV['OPEN_WEATHER_MAP_APP_ID'] }
 
       expect(response).to have_http_status(:ok)
-      expect(/[\d]+°C e [\w\s\p{L}]+ em [\w\s\p{L}]+ em (\d{2}\/\d{2}). Média para os próximos dias:( [\d]+°C em (\d{2}\/\d{2}),){4} [\d]+°C em (\d{2}\/\d{2})/).to match(JSON.parse(response.body)['message'])
+      expect(/[\d]+°C e [\w\s\p{L}]+ em [\w\s\p{L}]+ em (\d{2}\/\d{2}). Média para os próximos dias:( [\d]+°C em (\d{2}\/\d{2}),)+ [\d]+°C em (\d{2}\/\d{2})./).to match(JSON.parse(response.body)['message'])
     end
 
     it 'fail to create a tweet with an invalid city' do
